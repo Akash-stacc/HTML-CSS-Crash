155669fd6dc69e3284dfefc3e817b09b3836045d
Das-Akash2022 <www.beboosona2115@gmail.com>
2023-10-18 21:17:53 +0530

    added

---
+++ sandBox.js
@@ -0,0 +1,51 @@
+// // alert("hello world");
+// console.log(1);
+// console.log(2);
+// let age = 25;
+// let year = 1998;
+// console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// const points = 100;
+
+// console.log(points);
+
+// var score = 75;
+
+// console.log(score);
+
+// strings
+
+console.log("hello world");
+
+let email = "akash.networkenginer@gmail.com";
+console.log(email);
+
+// string concatenation
+
+let firstName = "Akash";
+let lastName = "Bapari";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+
+console.log(fullName[6]);
+
+// string length
+
+console.log(fullName.length);
+
+// string methods
+
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf(".");
+console.log(index);
